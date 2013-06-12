00185a93e72d72ecf4f7003c285c4714654c23fc
admin <>
2013-06-13 02:12:14 +1000

    added author & email to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ setup(
     name='numtowords',
     description='Converts numbers to words',
     long_description=open('README.md').read(),
+	
+	author='simonzack',
+	author_email='simonzack@gmail.com',
 
     packages=find_packages(),
 )
